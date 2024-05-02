21d1e05ecabb447cb5f82016340d71d883290c8a
matheusDev <matheus.info29@gmail.com>
2024-05-02 13:54:04 -0300

    Update

---
+++ jogadores_historicos_do_basquete/android/app/src/main/kotlin/com/example/jogadores_historicos_do_basquete/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jogadores_historicos_do_basquete
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ jogadores_historicos_do_basquete/lib/Screens/login_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), labelText: 'Login'),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
